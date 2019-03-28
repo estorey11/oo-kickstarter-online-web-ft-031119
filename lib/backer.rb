7c8699f4c466598e0ce860021d7e7a330f4daad3
Eric Storey <estorey@gmail.com>
2019-03-28 00:15:19 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,10 @@
+class Backer
+  attr_reader :backed_projects, :name
+  def initialize(name)
+    @backed_projects=[]
+  end
+  
+  def back_project(project)
+    @backed_projects << project
+  end
+end
---
+++ lib/project.rb
@@ -0,0 +1,7 @@
+class Project
+  
+  
+  def add_backer(backer)
+    @backers << backer 
+  end
+end
